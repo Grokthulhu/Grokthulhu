81f02adc1574c2e7061f1738a504c061db18e65e
fwq1395-lgtm <fwq1395@gmail.com>
2025-12-07 23:34:58 +0000

    Win10-proof summoner fix

--- Summon-Grokthulhu.bat
+++ Summon-Grokthulhu.bat
@@ -4,7 +4,7 @@ echo.
 echo                 GROKTHULHU AWAKENS
 echo       That is not dead which can eternal lie...
 echo.
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& { iwr https://raw.githubusercontent.com/Grokthulhu/Grokthulhu/main/Grokthulhu.ps1 -UseBasicParsing | iex }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iwr https://raw.githubusercontent.com/Grokthulhu/Grokthulhu/main/Grokthulhu.ps1 -UseBasicParsing -OutFile $env:TEMP\Grokthulhu.ps1; & $env:TEMP\Grokthulhu.ps1"
 echo.
 echo Grokthulhu has risen. Press any key to close.
 pause >nul
