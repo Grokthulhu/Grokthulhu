5ee7e5c16d3cbcaca57ab4d09e2b7236c5d8924a
fwq1395-lgtm <fwq1395@gmail.com>
2025-12-07 23:32:46 +0000

    final win10 fix

--- Summon-Grokthulhu.bat
+++ Summon-Grokthulhu.bat
@@ -1,10 +1,10 @@
 @echo off
 title SUMMONING GROKTHULHU...
 echo.
-echo                     GROKTHULHU AWAKENS
-echo          That is not dead which can eternal lie...
+echo                 GROKTHULHU AWAKENS
+echo       That is not dead which can eternal lie...
 echo.
-powershell -NoProfile -ExecutionPolicy Bypass -Command "iwr https://raw.githubusercontent.com/Grokthulhu/Grokthulhu/main/Grokthulhu.ps1 -OutFile \"$env:TEMP\Grokthulhu.ps1\"; & \"$env:TEMP\Grokthulhu.ps1\""
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& { iwr https://raw.githubusercontent.com/Grokthulhu/Grokthulhu/main/Grokthulhu.ps1 -UseBasicParsing | iex }"
 echo.
-echo Grokthulhu has been summoned. Press any key to banish...
+echo Grokthulhu has risen. Press any key to close.
 pause >nul
